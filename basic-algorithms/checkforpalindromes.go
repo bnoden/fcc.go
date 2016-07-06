bf963bc57abe1c409a8802415c12d7a50f789a6f
bnoden <brandroid369@gmail.com>
2016-07-06 05:57:20 -0500

    update

--- basic-algorithms/checkforpalindromes.go
+++ basic-algorithms/checkforpalindromes.go
@@ -1,34 +1,52 @@
+// https://play.golang.org/p/DmMymFUaNw
 package main
-// Needs work
-
-
-
-
-
 
 import (
-    "fmt"
-    "regexp"
-    "strings"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
 )
 
 func palindrome(str string) bool {
-    var bnoden string = strings.NewReplacer(`w, "")
-    result := bnoden.Replace(str)
-    return result
+	reg, err := regexp.Compile("[^A-Za-z0-9]+")
+	if err != nil {
+		log.Fatalln()
+	}
+
+	cleanstr := strings.ToLower(reg.ReplaceAllString(str, ""))
+	arr := []rune(cleanstr)
+	bnoden := []rune{}
+	isPalindrome := false
+	count := 0
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		bnoden = append(bnoden, arr[i])
+	}
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == bnoden[i] {
+			count++
+		}
+	}
+	if count == len(arr) {
+		isPalindrome = true
+	}
+	return isPalindrome
 }
+
 // Test
 func main() {
-    fmt.Println(palindrome("eye"))
-    fmt.Println(palindrome("_eye"))
-    fmt.Println(palindrome("race car"))
-    fmt.Println(palindrome("not a palindrome"))
-    fmt.Println(palindrome("A man, a plan, a canal. Panama"))
-    fmt.Println(palindrome("never odd or even"))
-    fmt.Println(palindrome("nope"))
-    fmt.Println(palindrome("almostomla"))
-    fmt.Println(palindrome("My age is 0, 0 si ega ym."))
-    fmt.Println(palindrome("1 eye for of 1 eye."))
-    fmt.Println(palindrome("0_0 (: /-\ :) 0-0"))
-    fmt.Println(palindrome("five|\_/|four"))
+	fmt.Println(palindrome("eye"))                            // true
+	fmt.Println(palindrome("_eye"))                           // true
+	fmt.Println(palindrome("race car"))                       // true
+	fmt.Println(palindrome("not a palindrome"))               // false
+	fmt.Println(palindrome("A man, a plan, a canal. Panama")) // true
+	fmt.Println(palindrome("never odd or even"))              // true
+	fmt.Println(palindrome("nope"))                           // false
+	fmt.Println(palindrome("almostomla"))                     // false
+	fmt.Println(palindrome("My age is 0, 0 si ega ym."))      // true
+	fmt.Println(palindrome("1 eye for of 1 eye."))            // false
+	// I used backticks instead of quotes to handle "unknown escape sequences". Still figuring out best way to deal with it.
+	fmt.Println(palindrome(`0_0 (: /-\ :) 0-0`)) // true
+	fmt.Println(palindrome(`five|\_/|four`))     // false
 }
--- basic-algorithms/js/03-check-palindromes.js
+++ basic-algorithms/js/03-check-palindromes.js
@@ -1,9 +1,9 @@
-function palindrome(str, bool, bnoden, nodenb, count) {
+function palindrome(str, bool, bnoden, nedonb, count) {
   bnoden = str.replace(/[^a-z0-9+]+/gi, '').toLowerCase().split(''),
-  	bool = false, nodenb = [], count = 0;
+  	bool = false, nedonb = [], count = 0;
 
-  for (var i = bnoden.length-1; i >= 0; i--) nodenb.push(bnoden[i]);
-  for (var i in bnoden) if (bnoden[i] === nodenb[i]) count++;
+  for (var i = bnoden.length-1; i >= 0; i--) nedonb.push(bnoden[i]);
+  for (var i in bnoden) if (bnoden[i] === nedonb[i]) count++;
 
   if (count === bnoden.length) bool = true
   
@@ -11,15 +11,15 @@ function palindrome(str, bool, bnoden, nodenb, count) {
 }
 
 // Test
-palindrome("eye")
-palindrome("_eye")
-palindrome("race car")
-palindrome("not a palindrome")
-palindrome("A man, a plan, a canal. Panama")
-palindrome("never odd or even")
-palindrome("nope")
-palindrome("almostomla")
-palindrome("My age is 0, 0 si ega ym.")
-palindrome("1 eye for of 1 eye.")
-palindrome("0_0 (: /-\ :) 0-0")
-palindrome("five|\_/|four")
+console.log(palindrome("eye")) // true
+console.log(palindrome("_eye")) // true
+console.log(palindrome("race car")) // true
+console.log(palindrome("not a palindrome")) // false
+console.log(palindrome("A man, a plan, a canal. Panama")) // true
+console.log(palindrome("never odd or even")) // true
+console.log(palindrome("nope")) // false
+console.log(palindrome("almostomla")) // false
+console.log(palindrome("My age is 0, 0 si ega ym.")) // true
+console.log(palindrome("1 eye for of 1 eye.")) // false
+console.log(palindrome("0_0 (: /-\ :) 0-0")) // true
+console.log(palindrome("five|\_/|four")) // false
