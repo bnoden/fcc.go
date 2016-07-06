c09be99ca310d1890622a3057746e239478c02d7
bnoden <brandroid369@gmail.com>
2016-07-06 03:35:37 -0500

    update

--- basic-algorithms/findlongestword.go
+++ basic-algorithms/findlongestword.go
@@ -1,5 +1,4 @@
-// needs work
-
+// https://play.golang.org/p/hXf_wdZdrC
 package main
 
 import (
@@ -11,7 +10,7 @@ func findLongestWord(str string) int {
 	bnoden := 1
 	arr := strings.Fields(str)
 
-	for i := 0; i <= len(arr); i++ {
+	for i := 0; i < len(arr); i++ {
 		if len(arr[i]) > bnoden {
 			bnoden = len(arr[i])
 		}
@@ -21,5 +20,9 @@ func findLongestWord(str string) int {
 
 // Test
 func main() {
-	fmt.Println(findLongestWord("The quick brown fox jumped over the lazy dog"))
+	fmt.Println(findLongestWord("The quick brown fox jumped over the lazy dog"))                 // 6
+	fmt.Println(findLongestWord("May the force be with you"))                                    // 5
+	fmt.Println(findLongestWord("Google do a barrel roll"))                                      // 6
+	fmt.Println(findLongestWord("What is the average airspeed velocity of an unladen swallow"))  // 8
+	fmt.Println(findLongestWord("What if we try a super-long word such as otorhinolaryngology")) // 19
 }
